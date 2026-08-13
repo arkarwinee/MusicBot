0503057d91b976397be3dcd9b0d813ee44211ca4
Ar Kar Wine <arkar.winee@gmail.com>
2026-08-13 11:44:11 +0630

    Downloading info

--- anony/plugins/play.py
+++ anony/plugins/play.py
@@ -120,7 +120,12 @@ async def play_hndlr(
         if Path(fname).exists():
             file.file_path = fname
         else:
-            await sent.edit_text(m.lang["play_downloading"])
+            await sent.edit_text(m.lang["play_downloading"].format(
+                    file.url,
+                    file.title,
+                    file.duration,
+                    file.user,
+                ))
             file.file_path = await yt.download(file.id, video=video)
 
     await anon.play_media(chat_id=m.chat.id, message=sent, media=file)
